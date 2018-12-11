bc7809a719bc00a1e78f692e78d86a6b359a62d4
Marcelo <marcelofreitas21@hotmail.com>
2018-12-11 18:40:40 -0200

    Atualização Nome de Teste

--- app/Http/Controllers/OrcamentoDetalheController.php
+++ app/Http/Controllers/OrcamentoDetalheController.php
@@ -25,7 +25,7 @@ class OrcamentoDetalheController extends Controller
         return view("orcamento-detalhe",['atv'=>$atv,'eod'=>$eod,'user'=>$user,'eo'=>$eo]);
     }
     public function salvar(Request $request){
-        $teste ='a';
+        $teste ="marcelo";
         $od = new OrcamentoDetalhe();
         $tipo = "error";
 
